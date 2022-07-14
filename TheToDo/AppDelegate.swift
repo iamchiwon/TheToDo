43f7942435c6874f9ce36a90bf3a169601714d87
Song Chiwon <iamchiwon@gmail.com>
2022-07-15 02:35:54 +0900

    Repository, ToDoService 구현체 IoC 에 등록하기
    - Swinject 의 Container 에 Reposity, ToDoService 를 등록합니다.
    - ToDoServiceImpl 을 생성하기 위해서는 Repository 가 필요하기 때문에 container 로부터 꺼내서 생성자 주입을 해줍니다.

--- TheToDo/AppDelegate.swift
+++ TheToDo/AppDelegate.swift
@@ -12,6 +12,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let container = Container()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        container.register(Repository.self) { _ in UserDefaultRepository() }
+        container.register(ToDoToggableService.self) { c in
+            let repository = c.resolve(Repository.self)!
+            return ToDoServiceImpl(repository: repository)
+        }
+
         return true
     }
 
