388ae061704900e21ba7bf39e1928b72cf3abbc6
Song Chiwon <iamchiwon@gmail.com>
2022-07-15 01:52:18 +0900

    ToDoService 떡밥 회수하기 준비
    - ToDoService 프로토콜을 구현하는 구현체를 만들 준비를 해봅시다.

---
+++ TheToDo/Domain/ToDoServiceImpl.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoServiceImpl.swift
+//  TheToDo
+//
+//  Created by Chiwon Song on 2022/07/15.
+//
+
+import Foundation
+
+class ToDoServiceImpl: ToDoService {
+    func create(title: String) {
+    }
+
+    func count() -> Int {
+    }
+
+    func item(at: Int) -> ToDo {
+    }
+}
